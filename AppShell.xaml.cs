9aa9a463626c121a32d595c09016816b60111762
Montso <Montso@TALLY>
2022-05-29 23:21:16 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MonkeyFinderApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ View/DetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MonkeyFinderApp.View;
+
+public partial class DetailsPage : ContentPage
+{
+	public DetailsPage()
+	{
+		InitializeComponent();
+	}
+}
